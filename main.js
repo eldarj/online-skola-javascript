20493f1015173ea60ef0d670556c032251fc0239
Eldar Jahijagic <eldar.jahijagic@infobip.com>
2021-01-18 18:36:36 +0100

    JSON, localstorage

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+function ukloniOsobu(key) {
+    console.log(key);
+
+    // TODO
+    // - ukloni osobu iz localstorage
+    localStorage.removeItem(key);
+
+    // 1. refreshaj page
+    location.reload();
+
+    // 2. refreshaj tabelu (sa npr. table.removeRow)
+}
+
 var besim = {};
 
+// TODO: Izbrisi ovo hard-kodirano osobe, već koristi sve iz localStorage
 besim.ime = 'Besim';
 besim.prezime = 'Dugalic';
 besim.adresa='Slavinovici';
@@ -13,9 +27,35 @@ console.log(eldar);
 var osobe = [besim];
 osobe.push(eldar);
 console.dir(osobe);
+
 var dataTableElement = document.getElementById('data-div');
+
 console.dir(dataTableElement);
+
 var div = document.getElementById( 'data-div' );
+
 osobe.forEach(element => {
-    dataTableElement.innerHTML += '<tr>' + '<td>' + element.ime + '</td>' + '<td>' + element.prezime + '</td>' + '<td>' + element.adresa + '</td>'+ '</tr>';
+    dataTableElement.innerHTML += '<tr>' 
+    + '<td>' + element.ime + '</td>' 
+    + '<td>' + element.prezime + '</td>' 
+    + `<td>${element.adresa}</td>`
+    + `<td><button onclick="document.location=\'Edit.html\'">Edit</button> <button onclick="ukloniOsobu('${element.ime}')">Delete</button></td>`
+    + '</tr>';
 });
+
+
+// TODO: Promijeni iz getItem("Eldar") u getItem("sveOsobe")
+// DOHVATI SVE OSOBE IZ LOCAL STORAGE
+var eldarJson = localStorage.getItem("Eldar"); // KAD GOD DOHVATAMO PODATKE IZ LOCALSTORAGE; KORISTIMO JSON.parse
+var eldar = JSON.parse(eldarJson);
+console.log('SVE OSOBE IZ LOCAL STORAGE');
+console.dir(eldar);
+
+// PRIKAZI U HTML (TODO: PRomijeni da radi sa nizom "sveOsobe" a ne "eldar")
+dataTableElement.innerHTML += '<tr>'
++ '<td>' + eldar.ime + '</td>' 
++ '<td>' + eldar.prezime + '</td>' 
++ '<td>' + eldar.adresa + '</td>'
++ `<td><button onclick="document.location=\'Edit.html\'">Edit</button> <button onclick="ukloniOsobu('${eldar.ime}')">Delete</button></td>`
++ '</tr>';
+
---
+++ nova-forma.js
@@ -0,0 +1,42 @@
+function submitForm() {
+    var forma = document.getElementById("forma-nova-osoba");
+    console.dir(forma);
+    var novaOsoba = {};
+    novaOsoba.ime = forma.elements.imeOsobe.value;
+    novaOsoba.prezime = forma.elements.prezimeOsobe.value;
+    novaOsoba.adresa = forma.elements.adresaOsobe.value;
+
+    console.log(novaOsoba);
+
+    var success = saveToLocalStorage(novaOsoba.ime, novaOsoba)
+
+    if (success === true) {
+        alert('Uspjesno ste snimili osobu.');
+    } else {
+        alert('Doslo je do greske');
+    }
+}
+
+
+function saveToLocalStorage(ime, cijeliObject) {
+    localStorage.setItem(ime, JSON.stringify(cijeliObject));
+    return true;
+}
+
+// TODO:
+// 1. Snimi niz osoba pod key = "sveOsobe" u localStorage
+// ---- dohvati sve postojece osobe (JSON.parse)
+// ---- dodaj novu osobu u postojeci niz
+// ---- snimi taj cijeli niz ponovo pod isti key = "sveOsobe" (JSON.stringify)
+
+/*
+Map localStorage = 
+{{
+    "eldar" -> {ime: ..., prezime: ..., adresa: ...}
+}}
+*/
+
+
+// JSON je string reprezentacija bilo kojeg objekta u Javascript ---- localStorage radi samo sa Stringom 
+// -- stringify pretvara bilo sta u String
+// -- parse() pretvara bilo sta u objekat
