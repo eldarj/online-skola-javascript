76aea9f443876e8bd4541e4815fd7383e32e1625
Eldar Jahijagic <eldar.jahijagic@infobip.com>
2021-01-18 17:40:35 +0100

    Nova verzija (2)

--- main.js
+++ main.js
@@ -1,52 +1,21 @@
-// Objekti i console log
-// 1. FUNKCIONALNOST (Javascript)
-// 2. BAZU PODATAKA (SQL, MongoDB, Local Storage)
-// 3. ISPIŠEŠ PODATKE NA STRANICU (document getElementById --- DOM)
 var besim = {};
 
 besim.ime = 'Besim';
 besim.prezime = 'Dugalic';
-
-var eldar = {}; // new Object() === {}
-
-eldar.ime = 'Eldar';
-eldar.prezime = 'Jahijagic';
-
-
+besim.adresa='Slavinovici';
 console.log(besim);
-console.log(eldar);
+var eldar={};
 
+eldar.ime='Eldar';
+eldar.prezime='Jahijagic';
+eldar.adresa='Brcanska Malta';
+console.log(eldar);
 var osobe = [besim];
 osobe.push(eldar);
-
-
-// 2. console.log
-console.log('This is console log.');
 console.dir(osobe);
-
-// 3. document.getElementById
 var dataTableElement = document.getElementById('data-div');
-dataTableElement.style.backgroundColor = 'white';
 console.dir(dataTableElement);
-
 var div = document.getElementById( 'data-div' );
-div.onmouseover = function() {
-  this.style.backgroundColor = 'green';
-};
-
-// for petlja
-// for (var i = 0; i < osobe.length; i++) {
-//     var j = osobe[i];
-//     dataTableElement.innerHTML += '<p>' + j.ime + ' ' + j.prezime + '</p>';
-// }
-
-// foreach petlja
 osobe.forEach(element => {
-    dataTableElement.innerHTML += '<tr>' + '<td>' + element.ime + '</td>' + '<td>' + element.prezime + '</td>' + '</tr>';
+    dataTableElement.innerHTML += '<tr>' + '<td>' + element.ime + '</td>' + '<td>' + element.prezime + '</td>' + '<td>' + element.adresa + '</td>'+ '</tr>';
 });
-
-
-
-// Javascript
-// -- ispisat podatke
-// -- promijenit CSS
